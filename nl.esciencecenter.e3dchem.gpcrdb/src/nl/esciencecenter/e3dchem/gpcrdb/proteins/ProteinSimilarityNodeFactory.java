72737ca5d66a6f7fba281e8172703a688fe41051
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2016-04-18 16:43:29 +0200

    Added protein similarity node.
    Refs #5

---
+++ nl.esciencecenter.e3dchem.gpcrdb/src/nl/esciencecenter/e3dchem/gpcrdb/proteins/ProteinSimilarityNodeFactory.java
@@ -0,0 +1,49 @@
+package nl.esciencecenter.e3dchem.gpcrdb.proteins;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "ProteinSimilarity" Node. Get a list of structures of a protein
+ */
+public class ProteinSimilarityNodeFactory extends NodeFactory<ProteinSimilarityNodeModel> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ProteinSimilarityNodeModel createNodeModel() {
+        return new ProteinSimilarityNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new ProteinSimilarityNodeDialog();
+    }
+
+    @Override
+    public NodeView<ProteinSimilarityNodeModel> createNodeView(int viewIndex, ProteinSimilarityNodeModel nodeModel) {
+        return new ProteinSimilarityNodeView(nodeModel);
+    }
+
+}
